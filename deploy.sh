8d15006a2444fbb33db74ecbe67597b12b856e36
Mark R Ohnsman <MarkOhnsman@users.noreply.github.com>
2020-11-03 11:22:53 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,5 @@ docker build -t TAG_NAME ./bin/Release/netcoreapp3.1/publish
 docker tag TAG_NAME registry.heroku.com/HEROKU_APP_NAME/web
 docker push registry.heroku.com/HEROKU_APP_NAME/web
 heroku container:release web -a HEROKU_APP_NAME
+echo press any key
+read end
